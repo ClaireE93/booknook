4c6466a1512b8dddb5df64222ce31b244f28869e
Claire Johnson <c.eliseee@gmail.com>
2017-09-26 10:28:33 -0700

    Revert "Bad commit"
    This reverts commit 2e214534395d95d6a951065d6882227c6b26d9c7.

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 const BookComponent = (props) => {
   let img;
-  let textClass;
   if (props.isLink) {
     img = <div><a href={props.book.url} target='_blank'><img className='search-image' src={props.book.image}/></a></div>
-    textClass = 'search-details-white'
   } else {
     img = <div><img className='search-image' src={props.book.image}/></div>
-    textClass = 'search-details-black'
   }
 
   return (
     <div className='search-item-container' onClick={() => (props.onClick(props.book))}>
       {img}
-      <div className={textClass}>
+      <div className='search-details'>
         <div className='search-title'>{props.book.title}</div>
         <div className='search-author'>{props.book.author}</div>
       </div>
--- src/Recommendations.js
+++ src/Recommendations.js
@@ -6,7 +6,7 @@ class Recommendations extends React.Component {
   render() {
     return (
       <div className={this.props.visible ? 'rec-show' : 'rec-hide'}>
-        <div className='recommendation-container'>
+        <div className='recommendation-container' >
           {this.props.books.map((book) => (
             <BookComponent key={book.ASIN} book={book} onClick={() => {return;}} isLink={true}/>
           ))}
