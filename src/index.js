3104122495728cfad6d2f0a00914bb70255833a5
Claire Johnson <c.eliseee@gmail.com>
2017-09-26 13:30:52 -0700

    Cleaning up code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-const data = [
-  {title: 'Frankenstein', author: 'Mary Shelley'},
-  {title: 'The Stranger', author: 'Albert Camus'},
-  {title: 'Catch 22', author: 'Joseph Heller'}
-];
-
 ReactDOM.render(
-  <App books={data}/>,
+  <App/>,
   document.getElementById('app')
 );
